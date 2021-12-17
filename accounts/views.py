22c0a8ba59a8e5efee986770d38ec7f43e9d22da
Kim-Jeong-Seok <js960426@gmail.com>
2021-12-17 16:42:11 +0900

    프로젝트 합침,회원가입체크박스,캘린더UI,modal창 데이터받기,소비top5, 건수top5 도넛그래프추가(21.12.17)

--- accounts/views.py
+++ accounts/views.py
@@ -48,6 +48,7 @@ def main(request):
 def home(request):
     return render(request, 'main.html')
 
+
 def calendar(request):
     #user = request.user.user_id
     # events = AccountBook.objects.filter(user_id=user )
@@ -87,13 +88,106 @@ def calendar(request):
     method_amount = spend_month_filter2.values('card').annotate(amount=Sum('amount')).order_by('-amount')[:5]
     # 소비 TOP5 거래처
     area_amount = spend_month_filter2.values('place').annotate(amount=Sum('amount')).order_by('-amount')[:5]
+    # 요약 페이지_카테고리 건수별 TOP5
+    category_amount_count = spend_month_filter2.values('category').annotate(count=Count('category')).order_by('-count')[:5]
 
-    print('디테일데이 나와라 ->>>'+ str(spend_day_sum))
 
-    return render(request, 'calendar.html' ,{'Spend_day':spend_day_sum,'Income_day':income_day_sum,'Detail_month':detail_month,'detail_day':detail_day,
-                'events':events, 'Expenditure': spend_sum, 'Income': income_sum, 'Category': category_amount, 'Method': method_amount, 'Area': area_amount, 'month':month
-                ,'spend_day_sum2':spend_day_sum2, 'income_day_sum2':income_day_sum2})
+    category_amount_data = []
+    category_amount_label = []
+    category_count_data = []
+    category_count_label = []
+    for item in category_amount:
+        category_amount_data.append(item['amount'])
+        category_amount_label.append(item['category'])
+    for item in category_amount_count:
+        category_count_data.append(item['count'])
+        category_count_label.append(item['category'])
 
+
+    return render(request, 'calendar.html' ,{
+        'Spend_day':spend_day_sum,
+        'Income_day':income_day_sum,
+        'Detail_month':detail_month,
+        'detail_day':detail_day,
+        'events':events,
+        'Expenditure': spend_sum,
+        'Income': income_sum,
+        'Category': category_amount,
+        'Method': method_amount,
+        'Area': area_amount,
+        'month':month,
+        'spend_day_sum2':spend_day_sum2,
+        'income_day_sum2':income_day_sum,
+        'Category_amount_data': category_amount_data,
+        'Category_amount_labels': category_amount_label,
+        'Category_count_data': category_count_data,
+        'Category_count_label': category_count_label,
+        'Category_count': category_amount_count,
+        })
+
+'''
+def calendar(request):
+    user = request.user.user_id
+    now = datetime.datetime.now()
+    year = now.strftime('%Y')
+    month = now.strftime('%m')
+    events = AccountBook.objects.filter(user_id=user )
+    events = Income.objects.all().values("amount", "income_date" ,"kind").union(Spend.objects.all().values("amount","spend_date", "kind"))
+    # 내역 페이지_월별 기간 필터링
+    spend_month_filter = Spend.objects.filter(user_id=user, spend_date__year=year, spend_date__month=month).values('kind','spend_date','amount')
+    income_month_filter = Income.objects.filter(user_id=user, income_date__year=year, income_date__month=month).values('kind','income_date','amount')
+    # 내역 페이지_쿼리셋 union
+    detail_month = spend_month_filter.union(income_month_filter).order_by('-spend_date')
+    # 내역 페이지_일별 수입,지출값 합산
+    spend_day_sum2 = spend_month_filter.values('spend_date__day').annotate(amount=Sum('amount')).order_by('-spend_date__day').values('kind','spend_date','amount')
+    income_day_sum2 = income_month_filter.values('income_date__day').annotate(amount=Sum('amount')).order_by('-income_date__day').values('kind','income_date','amount')
+    spend_day_sum = spend_month_filter.values('spend_date__day').annotate(amount=Sum('amount')).order_by('-spend_date__day')
+    income_day_sum = income_month_filter.values('income_date__day').annotate(amount=Sum('amount')).order_by('-income_date__day')
+    # 요약 페이지_월별 기간 필터링
+    spend_month_filter2 = Spend.objects.filter(user_id=user, spend_date__year=year, spend_date__month=month)
+    income_month_filter2 = Income.objects.filter(user_id=user, income_date__year=year, income_date__month=month)
+    # 요약 페이지_월 총 수입, 지출
+    spend_sum = spend_month_filter2.aggregate(Sum('amount'))
+    income_sum = income_month_filter2.aggregate(Sum('amount'))
+    # 요약 페이지_카테고리 금액별 TOP5
+    category_amount_sum = spend_month_filter2.values('category').annotate(amount=Sum('amount')).order_by('-amount')[:5]
+    # 요약 페이지_카테고리 건수별 TOP5
+    category_amount_count = spend_month_filter2.values('category').annotate(count=Count('category')).order_by('-count')[:5]
+    # 요약 페이지_카드사 TOP5
+    card_amount = spend_month_filter2.values('card').annotate(amount=Sum('amount')).order_by('-amount')[:5]
+    # 요약 페이지_기업명 TOP5
+    place_amount = spend_month_filter2.values('place').annotate(amount=Sum('amount')).order_by('-amount')[:5]
+    category_amount_data = []
+    category_amount_label = []
+    category_count_data = []
+    category_count_label = []
+    for item in category_amount_sum:
+        category_amount_data.append(item['amount'])
+        category_amount_label.append(item['category'])
+    for item in category_amount_count:
+        category_count_data.append(item['count'])
+        category_count_label.append(item['category'])
+        
+    return render(request, 'calendar.html', {
+        'events':events,
+        'Detail_month':detail_month,
+        'Spend_day':spend_day_sum,
+        'Income_day':income_day_sum,
+        'Spend': spend_sum,
+        'Income': income_sum,
+        'Category_sum': category_amount_sum,
+        'Category_count': category_amount_count,
+        'Card': card_amount,
+        'Place': place_amount,
+        'month':month,
+        'spend_day_sum2':spend_day_sum2,
+        'income_day_sum2':income_day_sum2,
+        'Category_amount_data': category_amount_data,
+        'Category_amount_labels': category_amount_label,
+        'Category_count_data': category_count_data,
+        'Category_count_label': category_count_label,
+        })
+'''
 
 def add_calendar(request):
     if request.method == "POST":
@@ -132,12 +226,12 @@ def add_calendar(request):
 def ajax_pushdate(request):
     if request.method == "POST":
         test = request.POST.get("testtest", None)
-        print(str(test))
-        even = Please.objects.filter(start_date=test)
-        print(str(even))
-        evens = list(even.values('title'))
-        print(str(evens))
-        evens = {'msg1':evens}
+        spend = Spend.objects.filter(spend_date=test).values('kind','spend_date','amount','place')
+        income = Income.objects.filter(income_date=test).values('kind','income_date','amount','income_way')
+        detail_month = income.union(spend).order_by('kind')
+        even1 = list(detail_month.values('kind','income_date','amount'))
+        evens = {'msg1':even1}
+
         return JsonResponse(evens)
 
 @csrf_exempt
@@ -186,9 +280,11 @@ def signup(request):
                                             email=request.POST['email'],
                                             name=request.POST['name'],
                                             phonenumber=request.POST['phonenumber'],
-                                            money=request.POST['money'],
-                                            fakemoney=request.POST['fakemoney'],
-                                            moneydate=request.POST['moneydate'],)
+                                            invest=request.POST['invest'],
+                                            u_chk=request.POST['u_chk'],
+                                            e_chk=request.POST['e_chk'],
+
+                                            )
             auth.login(request, user)
             return redirect('/')
         return render(request, 'signup.html')
