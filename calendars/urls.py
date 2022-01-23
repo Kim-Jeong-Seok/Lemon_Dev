28060fa9f8638a1e0faf0482af68395bd142c86a
LIMJAEHUN <ijhgun007@naver.com>
2022-01-23 20:23:28 +0900

    lim 카테고리, 추가페이지 저장

--- calendars/urls.py
+++ calendars/urls.py
@@ -10,24 +10,21 @@ app_name = 'calendars'
 
 urlpatterns = [
 
-    path('', views.home, name='home'),
-    path('calendar', views.calendar, name='calendar'),
-    path('summary', views.summary, name='summary'), # 요약페이 지
-    path('recom', views.recom, name='recom'), # 추천페이지
-    path('list', views.list, name='list'), # 내역페이지
-    #path('calendar', views.list, name='list'),
-    # path('cal_list', views.cal_list, name='calendarList'),
+        path('', views.home, name='home'),
+        path('calendar', views.calendar, name='calendar'),
+        path('summary', views.summary, name='summary'), # 요약페이 지
+        path('recom', views.recom, name='recom'), # 추천페이지
+        path('listview', views.listview, name='listview'), # 내역페이지
+        path('detail_search', views.detail_search, name='detail_search'),
+        path('add_calendar/', views.add_calendar, name='add_calendar'),
+        path('edit_calendar/<str:kind>/<int:spend_id>/', views.edit_calendar, name='edit_calendar'),
+        path('sedit_calendar/<int:spend_id>', views.sedit_calendar, name='sedit_calendar'),
+        path('iedit_calendar/<int:spend_id>', views.iedit_calendar, name='iedit_calendar'),
+        path('category_detail/<int>', views.category_detail, name= 'category_detail'),
 
-    # path('addlist', views.addlist, name='addlist'),
-
-    path('add_calendar/', views.add_calendar, name='add_calendar'),
-    path('edit_calendar/<str:kind>/<int:spend_id>/', views.edit_calendar, name='edit_calendar'),
-    path('sedit_calendar/<int:spend_id>', views.sedit_calendar, name='sedit_calendar'),
-    path('iedit_calendar/<int:spend_id>', views.iedit_calendar, name='iedit_calendar'),
-    # path('invest', views.invest, name='invest'), #모의주식금액설정
-
-    path('ajax_pushdate/', views.ajax_pushdate, name='ajax_pushdate'),
-    path('all_events/', views.all_events, name='all_events'),
-    path('add_event', views.add_event, name='add_event'),
-    path('load_list', views.load_list, name='load_list'),
+        path('ajax_pushdate', views.ajax_pushdate, name='ajax_pushdate'),
+        path('all_events/', views.all_events, name='all_events'),
+        path('add_event', views.add_event, name='add_event'),
+        path('load_list', views.load_list, name='load_list'),
+        path('ajax_sendSMS', views.ajax_sendSMS, name='ajax_sendSMS'),
 ]
