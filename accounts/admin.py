e1185a7361de832a6e4c99a6117aa4c6654ffbe8
ljs5960 <koreastars1@naver.com>
2022-02-21 16:58:45 +0900

    Add columns to admin page
    Django admin 페이지 - 항목, 수정기능 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from accounts.models import user
+from django.forms import TextInput, Textarea
+from django.db import models
 
 
 @admin.register(user)
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
+
+    list_display = [
         'uid',
+        'username',
         'email',
         'phonenumber',
-    )
+        'invest',
+        'invest_date',
+        'pin',
+    ]
+    list_editable = [
+        'username',
+        'email',
+        'phonenumber',
+        'invest',
+        'invest_date',
+        'pin',
+    ]
+    search_fields = ['uid', 'username']
+    list_per_page = 20
